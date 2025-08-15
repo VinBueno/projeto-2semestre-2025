afdd5f5ad440f0350ef9e81cb3316782174c55e1
Vinbueno <Vinbueno@outlook.com>
2025-08-14 23:01:29 -0300

    14/08/25

---
+++ src/main/java/com/programacao/web/fatec/api_fatec/controller/ClienteController.java
@@ -0,0 +1,21 @@
+package com.programacao.web.fatec.api_fatec.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/cliente")
+public class ClienteController {
+    
+    @GetMapping("/testeCliente1")
+    public String testeCliente() {
+        return "Teste Client";
+    }
+
+    @GetMapping("/testeCliente2/{nome}")
+    public String testeCliente2(@PathVariable String nome) {
+        return nome;
+    }
+}
