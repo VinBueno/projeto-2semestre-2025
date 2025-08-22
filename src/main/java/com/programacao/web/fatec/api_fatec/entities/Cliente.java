f036506dae0d991009ded8653c6e30ae902d8a5c
Vinbueno <vinbueno@outlook.com>
2025-08-21 22:54:36 -0300

    Atualizacao de hoje

--- src/main/java/com/programacao/web/fatec/api_fatec/entities/Cliente.java
+++ src/main/java/com/programacao/web/fatec/api_fatec/entities/Cliente.java
@@ -1,34 +1,33 @@
 package com.programacao.web.fatec.api_fatec.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-
-@Entity
 public class Cliente {
+    
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-   
+    
     private String nome;
- 
+
     public Cliente() {
+        
     }
- 
+
+    public Cliente(Long cod, String nome) {
+        this.id = cod;
+        this.nome = nome;
+    }
+
     public Long getId() {
         return id;
     }
- 
+
     public void setId(Long id) {
         this.id = id;
     }
- 
+
     public String getNome() {
         return nome;
     }
- 
+
     public void setNome(String nome) {
         this.nome = nome;
     }
--- src/test/java/com/programacao/web/fatec/api_fatec/ApiFatecApplicationTests.java
+++ src/test/java/com/programacao/web/fatec/api_fatec/ApiFatecApplicationTests.java
@@ -10,4 +10,6 @@ class ApiFatecApplicationTests {
 	void contextLoads() {
 	}
 
-}
+}  
+
+
